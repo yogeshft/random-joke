629bdf37fe499c8f2867dcd96fcf3fca30c0e367
Yogesh Yadav <yadavyogesh404@gmail.com>
2023-09-24 14:35:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const jokeContainer = document.getElementById("joke");
+const btn = document.getElementById("btn");
+const url =
+  "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+
+let getJoke = function () {
+  jokeContainer.classList.remove("fade");
+  fetch(url)
+    .then((data) => data.json())
+    .then((item) => {
+      jokeContainer.innerHTML = `${item.joke}`;
+      jokeContainer.classList.add("fade");
+    });
+};
+btn.addEventListener("click", getJoke);
+getJoke();
